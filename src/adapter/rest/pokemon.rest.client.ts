d3b9e6b4b7889dd337656dc828d72ec2b3bab144
Maxi Britez <maxi.britez@redb.ee>
2022-01-25 13:06:43 -0300

    Add example

---
+++ src/adapter/rest/pokemon.rest.client.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { Pokemon } from "src/application/model/pokemon";
+import { PokemonRepository } from "src/application/port/out/pokemon.repository";
+import axios from "axios";
+import { PokemonConfigurarion } from "src/config/pokemon.configuration";
+
+@Injectable()
+export class PokemonRestClient implements PokemonRepository {
+
+    constructor(private readonly pokemonConfiguration: PokemonConfigurarion) {}
+    
+    findPokemonByName(name: string): Promise<Pokemon> {
+        console.log(`Attempt to get url ${this.pokemonConfiguration.url}${name}`)
+        return axios
+        .get(`${this.pokemonConfiguration.url}${name}`, { timeout: this.pokemonConfiguration.timeout })
+        .then( response => new Pokemon(response.data.id, response.data.name))
+    }
+    
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { PokemonController } from './adapter/controller/pokemon.controller';
 import { AppService } from './app.service';
+import { FindPokemonUseCase } from './application/usecase/find.pokemon.usecase';
+import { PokemonRestClient } from './adapter/rest/pokemon.rest.client';
+
+import { ConfigModule } from '@nestjs/config';
+import { PokemonConfigurarion } from './config/pokemon.configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController, PokemonController],
+  providers: [AppService, PokemonConfigurarion, PokemonRestClient, FindPokemonUseCase],
 })
 export class AppModule {}
